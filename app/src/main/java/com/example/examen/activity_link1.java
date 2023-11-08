efd624f453342b7103062caa1c9027f580435144
Nameless6661 <107296890+Nameless6661@users.noreply.github.com>
2023-11-08 11:44:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/examen/activity_link1.java
@@ -0,0 +1,15 @@
+package com.example.examen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class activity_link1 extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link1);
+
+    }
+}
---
+++ app/src/main/java/com/example/examen/activity_link3.java
@@ -0,0 +1,11 @@
+package com.example.examen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_link3 extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link3);
+    }
+}
